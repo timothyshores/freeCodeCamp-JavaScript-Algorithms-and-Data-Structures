853c28af7b1dc6c031ae2fbed8b148e089fe70dd
Timothy Shores <timothymshores@gmail.com>
2019-12-16 23:12:54 -0600

    Add Basic JavaScript: Generate Random Whole Numbers with JavaScript

--- Basic Javascript/Basic JavaScript: Generate Random Fractions with JavaScript/solution.js
+++ Basic Javascript/Basic JavaScript: Generate Random Fractions with JavaScript/solution.js
@@ -1,3 +1,5 @@
 // Create a function randomFraction that returns a random number between 0 (inclusive) to 1 (exclusive)
 
 const randomFraction = () => Math.random();
+
+const randomFraction = () => Math.floor(Math.random() * 28);
---
+++ Basic Javascript/Basic JavaScript: Generate Random Whole Numbers with JavaScript/solution.js
@@ -0,0 +1,3 @@
+// Create a function randomWholeNum() that generates and return a random whole number between 0 and 9
+
+const randomWholeNum = () => Math.floor(Math.random() * 10);
